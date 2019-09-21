049431e80accc0b71ec5536b9b67d2b05c31d1d6
dinvilliers <49453562+dinvilliers@users.noreply.github.com>
2019-09-22 00:34:48 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
   session_start();
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("localhost", "root", "", "slotify");
+	$con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "bffde41fe21608", "6916e9d6", "heroku_3a90902f753aa9e");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
