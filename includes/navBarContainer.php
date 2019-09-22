319495f2bf0e5fb9d14fcac9e951914e4946e46f
dinvilliers <49453562+dinvilliers@users.noreply.github.com>
2019-09-23 00:49:59 +0100

    Update navBarContainer.php

--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -22,7 +22,7 @@
       </div>
 
       <div class="navItem">
-        <span role ="link" tabindex="0" onclick="openPage('yourmusic.php')" class="navItemLink">Your Music</span>
+        <span role ="link" tabindex="0" onclick="openPage('yourMusic.php')" class="navItemLink">Your Music</span>
       </div>
 
       <div class="navItem">
