4f51046c6b561d751e46497d41b4b53d873d71f5
dinvilliers <49453562+dinvilliers@users.noreply.github.com>
2019-09-23 01:40:43 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,7 @@
 <?php
 	ob_start();
-  session_start();
+  	session_start();
 	$timezone = date_default_timezone_set("Europe/London");
-
-	 $dbhost = 'eu-cdbr-west-02.cleardb.net';
-         $dbuser = 'bffde41fe21608';
-         $dbpass = '6916e9d6';
-	 $dbname = 'heroku_3a90902f753aa9e';
 
 	$con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "bffde41fe21608", "6916e9d6", "heroku_3a90902f753aa9e");
 
