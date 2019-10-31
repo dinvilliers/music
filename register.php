686cb83e555fe8a739eff7453440c226dda848b6
dinvilliers <49453562+dinvilliers@users.noreply.github.com>
2019-10-31 13:13:23 +0000

    Update register.php

--- register.php
+++ register.php
@@ -17,7 +17,7 @@
 
 <html>
 <head>
-	<title>Welcome to Slotify!</title>
+	<title>Mozzarella Music</title>
 
 	<link rel="stylesheet" href="assets/css/register.css">
 
@@ -57,7 +57,7 @@
 							<p>
 								<?php echo $account->getError(Constants::$loginFailed); ?>
 								<label for="loginUsername">Username</label>
-								<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('loginUsername') ?>" required>
+								<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. kanye" value="<?php getInputValue('loginUsername') ?>" required>
 							</p>
 							<p>
 								<label for="loginPassword">Password</label>
@@ -80,19 +80,19 @@
 								<?php echo $account->getError(Constants::$usernameCharacters); ?>
 								<?php echo $account->getError(Constants::$usernameTaken); ?>
 								<label for="username">Username</label>
-								<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
+								<input id="username" name="username" type="text" placeholder="e.g. kanye" value="<?php getInputValue('username') ?>" required>
 							</p>
 
 							<p>
 								<?php echo $account->getError(Constants::$firstNameCharacters); ?>
 								<label for="firstName">First name</label>
-								<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" value="<?php getInputValue('firstName') ?>" required>
+								<input id="firstName" name="firstName" type="text" placeholder="e.g. Kanye" value="<?php getInputValue('firstName') ?>" required>
 							</p>
 
 							<p>
 								<?php echo $account->getError(Constants::$lastNameCharacters); ?>
 								<label for="lastName">Last name</label>
-								<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" value="<?php getInputValue('lastName') ?>" required>
+								<input id="lastName" name="lastName" type="text" placeholder="e.g. West" value="<?php getInputValue('lastName') ?>" required>
 							</p>
 
 							<p>
@@ -100,12 +100,12 @@
 								<?php echo $account->getError(Constants::$emailInvalid); ?>
 								<?php echo $account->getError(Constants::$emailTaken); ?>
 								<label for="email">Email</label>
-								<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email') ?>" required>
+								<input id="email" name="email" type="email" placeholder="e.g. kanye@kanyewest.com" value="<?php getInputValue('email') ?>" required>
 							</p>
 
 							<p>
 								<label for="email2">Confirm email</label>
-								<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2') ?>" required>
+								<input id="email2" name="email2" type="email" placeholder="e.g. kanye@kanyewest.com" value="<?php getInputValue('email2') ?>" required>
 							</p>
 
 							<p>
@@ -137,7 +137,7 @@
 						 	<ul>
 							<li>Discover music you'll fall in love with</li>
 							<li>Create your own playlists</li>
-							<li>Follow artists to keep up to date</li>
+							<li>Unlimited plays, skips and repeats</li>
 						 	</ul>
 
 					</div>
